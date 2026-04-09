a74ff10a632a0939c719400a9a3bd76b625e0043
FelipeSilva10 <felipeconceicaoax@gmail.com>
2026-04-09 16:20:48 -0300

    transição para monorepo - 2

--- packages/core-ide/src/index.ts
+++ packages/core-ide/src/index.ts
@@ -1,7 +1,4 @@
-declare module '*.css';
-
 export { IdeScreen } from './screens/IdeScreen';
 export { TeacherDashboard } from './screens/TeacherDashboard';
 export { StudentDashboard } from './screens/StudentDashboard';
 export { LoginScreen } from './screens/LoginScreen';
-import './App.css';
